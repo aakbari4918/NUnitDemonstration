b8437f07e5cf9ceb22f705a90053630a9e726a4e
Avani Akbari <avani@kamero.in>
2024-06-09 07:58:29 +0530

    Add project files.

---
+++ NunitDemonstration/Class1.cs
@@ -0,0 +1,20 @@
+﻿namespace NunitDemonstration
+{
+    public static class Triangle
+    {
+        public static string ValidTriangle(int firstAngle, int secondAngle, int
+        thirdAngle)
+        {
+            string result;
+            if ((firstAngle + secondAngle + thirdAngle) == 180) 
+            {
+                result = "The triangle is valid.";
+            }
+            else
+            {
+                result = "Avani's triangle is NOT valid.";
+            }
+            return result;
+        }
+    }
+}
---
+++ TestClass/Class1.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using NunitDemonstration;
+namespace TestClass
+{
+    [TestFixture]
+    public class TriangleTest1
+    {
+        // Avani's Test
+        [Test]
+        public void ValidTriangle_Input60and60and60_OutputValidTriangle()
+        {
+            // Arrange
+            int firstAngle = 60;
+            int secondAngle = 60;
+            int thirdAngle = 60;
+            string expected = "The triangle is valid.";
+            // Act
+            string actual = Triangle.ValidTriangle(firstAngle, secondAngle, thirdAngle);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
